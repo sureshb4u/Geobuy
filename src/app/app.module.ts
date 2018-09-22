386676dbfa6ce63618b016a243a1edede654f354
Suresh <smsureshb4u@gmail.com>
2018-09-22 21:06:09 +0530

    Compoent and Config services added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { AppComponentService } from './app-component.service';
+import { ApiConfigService } from './api-config.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -9,10 +12,11 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     MDBBootstrapModule.forRoot()
   ],
   schemas: [ NO_ERRORS_SCHEMA ],
-  providers: [],
+  providers: [ApiConfigService, AppComponentService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
