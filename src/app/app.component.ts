f825b19d56e4528104533c58e0392423050069e9
Suresh <smsureshb4u@gmail.com>
2018-09-22 21:06:51 +0530

    Master Category and category added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import{AppComponentService} from './app-component.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor(private appService: AppComponentService){
+
+  }
+  masterCategories;
+  categories = [];
+  ngOnInit() {
+    this.appService.getMasterCategory()
+    .subscribe(response => this.masterCategories = response) ;
+    this.appService.getCategories()
+    .subscribe(response => this.categories = response.data) ;
+  }
+
 
 }
