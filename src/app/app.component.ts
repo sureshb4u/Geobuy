2325772fe946a6032ff2bf7923193693332c4724
vignesh <viki19nesh@gmail.com>
2018-09-23 19:03:55 +0530

    trendings updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,30 @@ export class AppComponent {
   }
   masterCategories;
   categories = [];
+  trendings =[];
+  categoryNameMap={};
+
   ngOnInit() {
     this.appService.getMasterCategory()
-    .subscribe(response => this.masterCategories = response) ;
+    .subscribe(response => {
+      this.masterCategories = response
+    }) ;
     this.appService.getCategories()
-    .subscribe(response => this.categories = response.data) ;
+    .subscribe(response => {
+      this.categories = response.data
+      for(var i=0; i<this.categories.length; i++){
+        var subcategories = this.categories[i].subcategory;
+        for(var j=0; j<subcategories.length; j++){
+          this.categoryNameMap[subcategories[j].id] = subcategories[j].tittle;
+        }
+      }
+
+    }) ;
+    this.getTrendingProducts();
   }
 
+  getTrendingProducts() {
+    this.appService.getTrendings().subscribe(response => this.trendings = response);
+  }
 
 }
