9f9132c71fcd6992e4356d0f220ae014b35ffd37
Suresh <smsureshb4u@gmail.com>
2018-10-02 15:02:03 +0530

    details page and routing added Detail page navigation added

---
+++ src/app/global-details/global-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-global-details',
+  templateUrl: './global-details.component.html',
+  styleUrls: ['./global-details.component.scss']
+})
+export class GlobalDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
