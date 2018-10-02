9066bee235a15e5c8d3602c1165d48e56d92e615
vignesh <viki19nesh@gmail.com>
2018-10-02 14:24:24 +0530

    products load

--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders,HttpHandler } from '@angular/common/http';
+import {ApiConfigService} from './api-config.service';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor() { }
+  constructor(private apiConfig: ApiConfigService, private http : HttpClient) { }
+
+  getProducts(): Observable<any> {
+    var url = this.apiConfig.hostUrl+"getProductsByCategory";
+      console.log('url ::'+url );
+      return this.http.get<any>(url).pipe(
+        tap((res) => console.log(res))
+      );
+  }
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import{ ProductsService} from '../products.service';
+import{ HomeService} from '../home.service';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private productsService : ProductsService, private homeService: HomeService) { }
+
+  products =[];
+  masterCategories;
+  categories = [];
+  categoryNameMap={};
 
   ngOnInit() {
+    this.homeService.getMasterCategory()
+      .subscribe(response => this.masterCategories = response) ;
+      this.homeService.getCategories()
+      .subscribe(response => {
+        this.categories = response.data
+        for(var i=0; i<this.categories.length; i++){
+          var subcategories = this.categories[i].subcategory;
+          for(var j=0; j<subcategories.length; j++){
+            this.categoryNameMap[subcategories[j].id] = subcategories[j].tittle;
+          }
+        }
+      }) ;
+    this.getProducts();
   }
 
+  getProducts() {
+    this.productsService.getProducts().subscribe(response => this.products = response);
+  }
 }
